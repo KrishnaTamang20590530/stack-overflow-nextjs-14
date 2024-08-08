e03449850754d0e42877c0ba1205fc93072d704e
Krishna Tamang <tmgkrishna78@gmail.com>
2024-08-08 21:59:48 +1000

    setting layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,15 +7,46 @@ import {
 } from "@clerk/nextjs";
 import "./globals.css";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import { Metadata } from "next";
+
+const inter = Inter({
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-grotesk",
+});
+
+export const metadata: Metadata = {
+  title: "Dev overflow",
+  description: "A community driven platform for developers",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">This is the piece of text</h1>
           <SignedOut>
             <SignInButton />
           </SignedOut>
