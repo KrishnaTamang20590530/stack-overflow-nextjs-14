ca2e93197b7869323dc825923f728d7f23ebab6d
Krishna Tamang <tmgkrishna78@gmail.com>
2024-08-15 10:08:32 +1000

    desktop and mobile navbar created

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return <div></div>;
 };
 
 export default HomePage;
